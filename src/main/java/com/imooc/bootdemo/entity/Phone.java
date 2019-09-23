cfcf68cbd9bc6a1f055bc5961e8efbc9154317da
dikas2017 <meiwanjie@gmail.com>
2019-09-23 22:06:29 +0800

    引入Lombok

--- src/main/java/com/imooc/bootdemo/entity/Phone.java
+++ src/main/java/com/imooc/bootdemo/entity/Phone.java
@@ -1,5 +1,10 @@
 package com.imooc.bootdemo.entity;
 
+import lombok.Data;
+import lombok.experimental.Accessors;
+
+@Data
+@Accessors(chain = true)
 public class Phone {
 
     private Integer id;
@@ -7,44 +12,4 @@ public class Phone {
     private String model;
     private String os;
     private String osVersion;
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getBrand() {
-        return brand;
-    }
-
-    public void setBrand(String brand) {
-        this.brand = brand;
-    }
-
-    public String getModel() {
-        return model;
-    }
-
-    public void setModel(String model) {
-        this.model = model;
-    }
-
-    public String getOs() {
-        return os;
-    }
-
-    public void setOs(String os) {
-        this.os = os;
-    }
-
-    public String getOsVersion() {
-        return osVersion;
-    }
-
-    public void setOsVersion(String osVersion) {
-        this.osVersion = osVersion;
-    }
 }
